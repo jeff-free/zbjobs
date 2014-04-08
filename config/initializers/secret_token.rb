65c5d36e4312cd5bff90b016f15753674cd5df99
unlimitedlife <jeffrey410469@gmail.com>
2014-04-08 18:17:54 +0800

    rails g

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zbjobs::Application.config.secret_key_base = '2f78ca9f5d0e809559e19b4a778f4396f5488bd0551d12bec8c53f56bbbc49e0405216542b5bef5783f0e4c89cae54e9955b514cbec3f903b06345bc693878de'
