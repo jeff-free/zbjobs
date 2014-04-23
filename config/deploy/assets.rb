5057bfebd4532716586f74e33cfc4a50b2718446
unlimitedlife <jeffrey410469@gmail.com>
2014-04-23 22:22:45 +0800

    test

--- config/deploy/assets.rb
+++ config/deploy/assets.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 
 # from https://github.com/AF83/capistrano-af83/blob/master/lib/capistrano/af83/deploy/assets.rb
-set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
+set :assets_dependencies, %w(lib/assets vendor/assets Gemfile.lock config/routes.rb)
 
 namespace :deploy do
   namespace :assets do
