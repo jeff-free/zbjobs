673b5f7ec436870f4192c7d07368e532e7e8b082
unlimitedlife <jeffrey410469@gmail.com>
2014-05-06 00:32:30 +0800

    add facebook page

--- config/deploy/assets.rb
+++ config/deploy/assets.rb
@@ -1,46 +1,46 @@
-# -*- encoding : utf-8 -*-
+# # -*- encoding : utf-8 -*-
 
-# from https://github.com/AF83/capistrano-af83/blob/master/lib/capistrano/af83/deploy/assets.rb
-set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
+# # from https://github.com/AF83/capistrano-af83/blob/master/lib/capistrano/af83/deploy/assets.rb
+# set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
 
-namespace :deploy do
-  namespace :assets do
-
-    desc <<-DESC
-      Run the asset precompilation rake task. You can specify the full path \
-      to the rake executable by setting the rake variable. You can also \
-      specify additional environment variables to pass to rake via the \
-      asset_env variable. The defaults are:
-
-        set :rake,      "rake"
-        set :rails_env, "production"
-        set :asset_env, "RAILS_GROUPS=assets"
-        set :assets_dependencies, fetch(:assets_dependencies) + %w(config/locales/js)
-    DESC
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      begin
-        from = source.next_revision(current_revision)
-      rescue
-        err_no = true
-      end
-
-      if err_no || capture("cd #{latest_release} && #{source.local.diff(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-
-  end
-end
 # namespace :deploy do
 #   namespace :assets do
-#     desc 'Run the precompile task locally and rsync with shared'
-#     task :precompile, :roles => :web, except: { no_release: true } do
-#       %x{RAILS_ENV=production bundle exec rake assets:precompile}
-#       %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{domain}:#{shared_path}}
-#       %x{RAILS_ENV=production bundle exec rake assets:clean}
-#       %x{rm -rf public/assets}
+
+#     desc <<-DESC
+#       Run the asset precompilation rake task. You can specify the full path \
+#       to the rake executable by setting the rake variable. You can also \
+#       specify additional environment variables to pass to rake via the \
+#       asset_env variable. The defaults are:
+
+#         set :rake,      "rake"
+#         set :rails_env, "production"
+#         set :asset_env, "RAILS_GROUPS=assets"
+#         set :assets_dependencies, fetch(:assets_dependencies) + %w(config/locales/js)
+#     DESC
+#     task :precompile, :roles => :web, :except => { :no_release => true } do
+#       begin
+#         from = source.next_revision(current_revision)
+#       rescue
+#         err_no = true
+#       end
+
+#       if err_no || capture("cd #{latest_release} && #{source.local.diff(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
+#         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+#       else
+#         logger.info "Skipping asset pre-compilation because there were no asset changes"
+#       end
 #     end
+
 #   end
 # end
+# # namespace :deploy do
+# #   namespace :assets do
+# #     desc 'Run the precompile task locally and rsync with shared'
+# #     task :precompile, :roles => :web, except: { no_release: true } do
+# #       %x{RAILS_ENV=production bundle exec rake assets:precompile}
+# #       %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{domain}:#{shared_path}}
+# #       %x{RAILS_ENV=production bundle exec rake assets:clean}
+# #       %x{rm -rf public/assets}
+# #     end
+# #   end
+# # end
