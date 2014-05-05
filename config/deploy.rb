89df3cf6048b5af66f47de06ae7a757ad822198e
unlimitedlife <jeffrey410469@gmail.com>
2014-05-05 23:17:32 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,24 @@
+# require 'capistrano/ext/multistage'
+# require 'bundler/capistrano' #Using bundler with Capistrano
+# require 'capistrano/local_precompile'
+# require 'rvm/capistrano'
+# require 'cape'
+
+# set :stages, %w(staging production)
+# set :default_stage, "production"
+
+# Cape do
+#   mirror_rake_tasks :dev
+# end
 require 'capistrano/ext/multistage'
 require 'bundler/capistrano' #Using bundler with Capistrano
-require 'capistrano/local_precompile'
 require 'rvm/capistrano'
 require 'cape'
 
 set :stages, %w(staging production)
 set :default_stage, "production"
-
+  
+  
 Cape do
   mirror_rake_tasks :dev
 end
